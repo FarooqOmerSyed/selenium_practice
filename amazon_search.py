60fad9955fd6096591dbac8de1c220f7366ef3e9
OMER FAROOQ SYED <farooqsyedomer@hotmail.com>
2024-09-14 09:18:52 +0530

    search_auto

--- amazon_search.py
+++ amazon_search.py
@@ -3,22 +3,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Set up the WebDriver (make sure to have the appropriate driver for your browser)
 driver = webdriver.Firefox()
+url = 'https://www.amazon.in/'
 
-try:
-    # Open the website
-    driver.get('https://www.amazon.in/')  # Replace with the actual URL
+def search_amzn():
+    try:
+        driver.get(url)
+        search_box = driver.find_element(By.ID, 'twotabsearchtextbox')
+        search_box.clear()
+        search_box.send_keys('Lenovo Laptop')
+        search_box.send_keys(Keys.RETURN)
+        time.sleep(6)
+    finally:
+        driver.quit()
+    
+if __name__ == '__main__':
+    search_amzn()
+    
 
-    # Find the search box using its ID and enter the search term
-    search_box = driver.find_element(By.ID, 'twotabsearchtextbox')  # Replace 'search-box-id' with the actual ID
-    search_box.clear()
-    search_box.send_keys('bluetooth speaker')
-    search_box.send_keys(Keys.RETURN)
 
-    # Wait for a few seconds to see the results
-    time.sleep(5)
 
-finally:
-    # Close the browser
-    driver.quit()
+
+
