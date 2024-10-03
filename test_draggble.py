af80b14a044b85bf5942958799f3484e671e52a6
OMER FAROOQ SYED <farooqsyedomer@hotmail.com>
2024-10-03 12:49:42 +0530

    drag_drop_functional

--- test_draggble.py
+++ test_draggble.py
@@ -2,27 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import pytest
 
-# Replace with the path to your WebDriver (e.g., chromedriver)
-driver = webdriver.Firefox()
+global url 
+url = "https://practice.expandtesting.com/drag-and-drop"
 
-# Open the webpage
-driver.get("https://practice.expandtesting.com/drag-and-drop")
-driver.maximize_window()
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
+    driver.get(url)
+    driver.maximize_window()
+    time.sleep(2)
+    yield driver
+    driver.quit()
 
-# Locate draggable and droppable elements
-draggable_element = driver.find_element(By.ID, "column-a")
-droppable_element = driver.find_element(By.ID, "column-b")
-
-# Create an ActionChains object
-actions = ActionChains(driver)
-time.sleep(4)
-# Perform drag and drop
-actions.drag_and_drop(draggable_element, droppable_element).perform()
-
-
-
-# Close the browser
-driver.quit()
+def test_drag_and_drop(driver):
+    draggable_element = driver.find_element(By.ID, "column-a")
+    droppable_element = driver.find_element(By.ID, "column-b")
+    actions = ActionChains(driver)
+    time.sleep(4)
+    actions.drag_and_drop(draggable_element, droppable_element).perform()
 
 print("Drag and drop test completed!")
